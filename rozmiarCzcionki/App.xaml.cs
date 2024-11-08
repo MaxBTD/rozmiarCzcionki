15251f0b2b9aa5603e2b6d10ef0d05d55bfcbe69
5m <konradek982@gmail.com>
2024-11-08 12:16:51 +0100

    Dodaj pliki projektów.

---
+++ rozmiarCzcionki/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace rozmiarCzcionki
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ rozmiarCzcionki/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿namespace rozmiarCzcionki
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void sizeSldrMoved(object sender, EventArgs e)
+        {
+            textToChange.FontSize = sldrSize.Value;
+            lblFontSize.Text = $"Rozmiar czcionki: {sldrSize.Value.ToString("0.00")}";
+        }
+
+        private void radioTextAlign(object sender, EventArgs e) {
+            if (radioLeft.IsChecked)
+                textToChange.HorizontalTextAlignment = TextAlignment.Start;
+            else if(radioRight.IsChecked)
+                textToChange.HorizontalTextAlignment= TextAlignment.End;
+            else
+                textToChange.HorizontalTextAlignment= TextAlignment.Center;
+        }
+
+        private void btnChangeColorClicked(object sender, EventArgs e)
+        {
+            string[] colors = { "#32a852", "#6e2999", "#b32b2b", "#2b8cb3", "#ffffff" };
+
+            if (count < 4)
+                count++;
+            else
+                count = 0;
+
+            textToChange.TextColor = Color.FromHex(colors[count]);
+        }
+    }
+
+}
